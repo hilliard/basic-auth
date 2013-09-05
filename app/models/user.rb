997bd217f73eea1b1a1605f188c00acfe8028e2b
hilliard <hilliards@gmail.com>
2013-09-04 19:32:30 -0600

    Fixup user mailer and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,36 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :username, :password, :password_confirmation, :new_password, :new_password_confirmation, :remember_me
-  attr_accessor :password, :new_password, :remember_me
+  attr_accessor :password, :new_password, :previous_email, :previous_username, :remember_me
   before_save :encrypt_password
 
   validates_confirmation_of :password
-  validates_confirmation_of :new_password, :if => Proc.new {|user| !user.new_password.nil? && !user.new_password.empty? }
+
+  validates_confirmation_of :new_password, :if => Proc.new {|user|
+    !user.new_password.nil? && !user.new_password.empty? }
+
   validates_presence_of :password, :on => :create
-  validates_presence_of :email, :on => :create
-  validates_presence_of :username, :on => :create
-  validates_uniqueness_of :email
-  validates_uniqueness_of :username
+
   validates_presence_of :email, :if => Proc.new {|user|
     user.previous_email.nil? || user.email != user.previous_email}
+
   validates_presence_of :username, :if => Proc.new {|user|
     user.previous_username.nil? || user.username != user.previous_username}
+
   validates_uniqueness_of :email, :if => Proc.new {|user|
     user.previous_email.nil? || user.email != user.previous_email}
+
   validates_uniqueness_of :username, :if => Proc.new {|user|
     user.previous_username.nil? || user.username != user.previous_username}
 
   def initialize(attributes = {})
-    super # must allow the active record to initialize!
+    super     # must allow the active record to initialize!
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def update(attributes = {})
+    super     # must allow the active record to initialize!
     attributes.each do |name, value|
       send("#{name}=", value)
     end
@@ -43,6 +53,7 @@ class User < ActiveRecord::Base
       nil
     end
   end
+
   def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 BasicAuth::Application.routes.draw do
   root :to => 'home#index'
 
-  get "sign_in" => "authentication#sign_in"
   get "signed_out" => "authentication#signed_out"
-  get "change_password" => "authentication#change_password"
   get "forgot_password" => "authentication#forgot_password"
-  get "new_user" => "authentication#new_user"
   get "password_sent" => "authentication#password_sent"
+
+  get "sign_in" => "authentication#sign_in"
+  post "sign_in" => "authentication#login"
+
+  get "new_user" => "authentication#new_user"
+  post "new_user" => "authentication#register"
 
   get "account_settings" => "authentication#account_settings"
   put "account_settings" => "authentication#set_account_info"
 
   get "forgot_password" => "authentication#forgot_password"
   put "forgot_password" => "authentication#send_password_reset_instructions"
-
   get "password_reset" => "authentication#password_reset"
   put "password_reset" => "authentication#new_password"
-  post "sign_in" => "authentication#login"
-  post "new_user" => "authentication#register"
-
 
   get "admin_users" => "admin#users"
   delete "user/:id" => "admin#delete_user", :as => "user"
